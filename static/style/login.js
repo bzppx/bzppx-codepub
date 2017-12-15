e4e51c6d7f43fc1ea3a2c42bb8247f78d640aedc
phachon <phachon@163.com>
2017-12-15 15:56:05 +0800

    fix login

--- static/style/login.js
+++ static/style/login.js
@@ -1,14 +1,28 @@
 $(function() {
+
+    function errorMsg(msg) {
+        $(".error-box h1").text("登录出错："+msg);
+        $(".error-box").show();
+    }
     var refreshCode = function() {
         $(".captcha")[0].src = "/login/captcha?_=" + (Math.random() + "").substring(1)
-    }
+    };
     $(".captcha").click(refreshCode);
     $("#login-button").click(function(event) {
         event.preventDefault();
         var u = $("[name='username']").val();
         var p = $("[name='password']").val();
         var c = $("[name='captcha']").val();
-        if (!u || !p || !c) {
+        if (!u) {
+            errorMsg("用户名不能为空！");
+            return
+        }
+        if (!p) {
+            errorMsg("密码不能为空！");
+            return
+        }
+        if (!c) {
+            errorMsg("验证码不能为空！");
             return
         }
         $.post("", {
@@ -17,14 +31,16 @@ $(function() {
             "captcha": c,
         }, function(data) {
             if (data.code) {
+                $(".error-box").hide();
                 $('form').fadeOut(500, function() {
                     setTimeout(function() {
                         location = data.redirect.url;
                     }, data.redirect.sleep);
                 });
-                $('.wrapper').addClass('form-success');
+                $('.welcome-text').show();
             } else {
-                alert(data.message)
+                errorMsg(data.message);
+                // alert(data.message);
                 $("[name='captcha']").val("");
                 refreshCode();
             }
