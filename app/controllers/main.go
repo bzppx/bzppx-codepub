8eb682dc04b88fdcd4e6eddee56ae84859b20bb5
arraykeys@gmail.com <arraykeys@gmail.com>
2018-01-09 15:22:09 +0800

    index notice added
    Signed-off-by: arraykeys@gmail.com <arraykeys@gmail.com>

--- app/controllers/main.go
+++ app/controllers/main.go
@@ -1,5 +1,9 @@
 package controllers
 
+import (
+	"bzppx-codepub/app/models"
+)
+
 type MainController struct {
 	BaseController
 }
@@ -9,6 +13,12 @@ func (this *MainController) Index() {
 	this.viewLayoutTitle("CodePub POWVEREDBY BZPPX", "main/index", "main")
 }
 func (this *MainController) Default() {
+	var err error
+	this.Data["notices"], err = models.NoticeModel.GetNoticesByLimit(0, 5)
+	if err != nil {
+		this.ErrorLog("获取最新公告失败：" + err.Error())
+		this.viewError("获取最新公告失败")
+	}
 	this.viewLayoutTitle("首页", "main/default", "page")
 }
 func (this *MainController) Tpl() {
