a1f885473252a92b10a46c0458f004d68d99fd04
haha03942008 <648588267@qq.com>
2018-01-18 08:18:56 +0800

    fix bug

--- app/controllers/publish.go
+++ app/controllers/publish.go
@@ -342,5 +342,5 @@ func (this *PublishController) addTaskAndTaskLog(taskValue map[string]interface{
 	}
 
 	this.InfoLog("添加发布任务 " + taskIdStr + " 成功")
-	this.jsonSuccess("操作成功！", nil, "/task/taskLog?task_id="+taskIdStr)
+	this.jsonSuccess("发布成功！", nil, "/task/taskLog?task_id="+taskIdStr)
 }
