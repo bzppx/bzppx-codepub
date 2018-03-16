5461160c460a65f04238608066ee2849fbced605
phachon <phachon@163.com>
2018-03-16 14:55:16 +0800

    update pack.sh

--- pack.sh
+++ pack.sh
@@ -6,36 +6,38 @@ mkdir ${RELEASE}
 
 # windows amd64
 echo 'Start pack windows amd64...'
+GOOS=windows GOARCH=amd64 go get ./...
+GOOS=windows GOARCH=amd64 go build ./
 cd install
 GOOS=windows GOARCH=amd64 go build ./
 cd ..
-GOOS=windows GOARCH=amd64 go build ./
 tar -czvf "${RELEASE}/bzppx-codepub-windows-amd64.tar.gz" bzppx-codepub.exe conf/ static/ views/ install/install.exe LICENSE README.md
 rm -rf bzppx-codepub.exe
 
 echo 'Start pack windows X386...'
+GOOS=windows GOARCH=386 go get ./...
+GOOS=windows GOARCH=386 go build ./
 cd install
 GOOS=windows GOARCH=386 go build ./
 cd ..
-GOOS=windows GOARCH=386 go build ./
-
 tar -czvf "${RELEASE}/bzppx-codepub-windows-386.tar.gz" bzppx-codepub.exe conf/ static/ views/ install/install.exe LICENSE README.md
 rm -rf bzppx-codepub.exe
 
 echo 'Start pack linux amd64'
+GOOS=linux GOARCH=amd64 go get ./...
+GOOS=linux GOARCH=amd64 go build ./
 cd install
 GOOS=linux GOARCH=amd64 go build ./
 cd ..
-GOOS=linux GOARCH=amd64 go build ./
 tar -czvf "${RELEASE}/bzppx-codepub-linux-amd64.tar.gz" bzppx-codepub conf/ static/ views/ install/install LICENSE README.md
 rm -rf bzppx-codepub
 
 echo 'Start pack linux 386'
+GOOS=linux GOARCH=386 go get ./...
+GOOS=linux GOARCH=386 go build ./
 cd install
 GOOS=linux GOARCH=386 go build ./
 cd ..
-GOOS=linux GOARCH=386 go build ./
-
 tar -czvf "${RELEASE}/bzppx-codepub-linux-386.tar.gz" bzppx-codepub conf/ static/ views/ install/install LICENSE README.md
 rm -rf bzppx-codepub
 
